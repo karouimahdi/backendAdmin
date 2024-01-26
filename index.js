5644a8d24b01c23c84f5e45cb325dbeaeaedb276
krmahdi <mahdikaroui383@gmail.com>
2024-01-26 16:15:29 +0100

    fixing endpoints

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(logger('dev'));
 const corsOptions ={
 
   origin:'https://frontadmin-beta.vercel.app', 
-  //origin:'http://localhost:3002',
+
 
 
   credentials:true,            //access-control-allow-credentials:true
